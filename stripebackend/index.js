a2afa4342ddd3bb74ffc0b972bc2e1e1e968ff9a
aadarshp31 <adarshpandey.in@gmail.com>
2020-06-04 19:04:25 +0530

    Configured Stripe at the backend
    > Created a POST route for payment. \n > Using Stripe methods to create customers and charges with shipping address.

--- stripebackend/index.js
+++ stripebackend/index.js
@@ -16,6 +16,41 @@ app.get("/", (req, res) => {
     res.send("Express Server is running...")
 })
 
+app.post("/payment", (req, res) => {
+    const { product, token } = req.body;
+    console.log("PRODUCT", product);
+    console.log("PRICE", product.price);
+    //This key will be used to track a unique order
+    idempotencyKey = uuid();
+
+    //Stripe Create Customer
+    stripe.customers.create({
+        email: token.email,
+        source: token.id
+    })
+    .then(customer => {
+        stripe.charges.create({
+            //Compulsory
+            customer: customer.id,
+            amount: product.price * 100,
+            currency: "usd",
+            //Optional
+            recipient_email: customer.email,
+            description: `Purchase of product: ${product.name}`,
+            shipping: {
+                name: token.card.name,
+                address: {
+                    city: token.card.address_city,
+                    country: token.card.address_country
+                }
+            }
+        }, {idempotencyKey})
+    })
+    .then(result => res.status(200).json(result))
+    .catch(err => console.log(err))
+
+})
+
 //listen
 const port = process.env.PORT
 app.listen(port, () => {
