eec09234c00224b79514aefcbaa0d0132f21e503
aadarshp31 <adarshpandey.in@gmail.com>
2020-06-04 22:45:30 +0530

    Integrated Pay with Stripe button with react-stripe-checkout npm package component.

--- stripefrontend/src/App.js
+++ stripefrontend/src/App.js
@@ -1,24 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import StripeCheckout from "react-stripe-checkout";
 
 function App() {
+  const initialValues = {
+    name: "React Crash Course 2020",
+    price: 10,
+    productBy: "learncodeonline.in"
+  }
+
+  const [ product, setProduct ] = useState(initialValues);
+
+
+
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <StripeCheckout 
+          stripeKey={process.env.REACT_APP_STRIPE_KEY}
+          token=""
+          name="Buy Course Stripe Component"
+          amount={product.price * 100}
+         >
+          <button className="waves-effect waves-light btn-large">Purchase with Stripe for ${product.price}</button>
+         </StripeCheckout>
       </header>
     </div>
   );
